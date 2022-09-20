ffec7b86f6ead0c47e3c3f126524bd53b4333074
NewBoss1 <58598186+NewBoss1@users.noreply.github.com>
2022-09-20 23:29:19 +0900

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const title = document.querySelector(".hello h1");
+//document - html문서
+console.log(title);
+
+//맨 처음 것만 출력
+
+//3개를 다 가져오고 싶다면 querySelsctorAll을 써주면 됨
+//querySelectorAll은 selector 안의 조건에 부합하는 것들을 가져다줌
